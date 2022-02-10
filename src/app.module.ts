54043ae99ce7e80a8280823e03483aaded0a5d4c
Faizanshaikh12 <faizanroy000777@gmail.com>
2022-02-11 00:07:17 +0530

    db configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { FeedModule } from './feed/feed.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PG_HOST,
+      port: parseInt(<string>process.env.PG_PORT),
+      username: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    FeedModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
